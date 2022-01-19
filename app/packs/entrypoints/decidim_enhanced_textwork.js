2bd9b731faa24f12fc108a5adfd6277e61835026
Alexander Rusa <alex@rusa.at>
2022-01-19 08:29:27 +0100

    Fix and improve layout for paragraphs

--- app/packs/entrypoints/decidim_enhanced_textwork.js
+++ app/packs/entrypoints/decidim_enhanced_textwork.js
@@ -1,8 +1,17 @@
 import "src/decidim/enhanced_textwork/utils"
 import "src/decidim/enhanced_textwork/add_paragraph"
+import "src/decidim/enhanced_textwork/toggle_element.js"
 
 // Images
 require.context("../images", true)
 
 // Documents
 require.context("../documents", true)
+
+// Add proposals-overview class to elements with the class "off-canvas-wrapper"
+$(() => {
+  var elements = document.getElementsByClassName("off-canvas-wrapper");
+  for (let element of elements) {
+    element.classList.add("paragraphs-overview");
+  }
+});
