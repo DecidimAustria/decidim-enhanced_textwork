8e9047de891528302f06b64088528a7e7c64f893
Alexander Rusa <alex@rusa.at>
2022-02-17 18:24:42 +0100

    Bump version to v1.0.5

--- lib/decidim/enhanced_textwork/version.rb
+++ lib/decidim/enhanced_textwork/version.rb
@@ -4,7 +4,7 @@ module Decidim
   # This holds decidim-paragraphs version.
   module EnhancedTextwork
     def self.version
-      "1.0.4"
+      "1.0.5"
     end
 
     def self.compat_decidim_version 
